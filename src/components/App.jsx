7c44bedba484fe66d3fd5119124101635cbce512
serg-svy <97796536+serg-svy@users.noreply.github.com>
2023-10-17 13:57:47 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { Product } from './Product';
+
 const productsData = [
   {
     id: '3',
@@ -19,33 +21,38 @@ const productsData = [
   },
 ];
 
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+];
+
 export const App = () => {
   return (
     <section>
       <h1>Hello FSon89🥳</h1>
 
-      <Product title="Tacos With Lime" price="10.99" discount={null} />
-      <Product title="Tacos L" price="7.99" discount={1.5} />
-      <Product title="Tacos S" price="4.99" discount={2.33} />
+      <ul>
+        {months.map(month => (
+          <li key={month}>{month}</li>
+        ))}
+      </ul>
+
+      {productsData.map(product => {
+        return (
+          <Product
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            discount={product.discount}
+          />
+        );
+      })}
     </section>
-  );
-};
-const Product = ({ title, price, discount }) => {
-  return (
-    <div>
-      <img
-        src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        alt="Tacos With Lime"
-        width="640"
-      />
-      <h2>{title}</h2>
-      {discount ? (
-        <h3> Discount: {discount}$</h3>
-      ) : (
-        <p>Sorry, but discount on this type of product has expired !</p>
-      )}
-      <p>{price}$</p>
-      <button type="button">Add to cart</button>
-    </div>
   );
 };
