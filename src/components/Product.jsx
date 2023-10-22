7c6d3eade42fd39e058daa4a02d2db689bd96074
serg-svy <97796536+serg-svy@users.noreply.github.com>
2023-10-22 14:53:00 +0300

    push

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,28 +1,30 @@
+import './product.css';
+
 export const Product = ({ title, price, discount }) => {
-  const productBg = discount ? 'rgb(151,230,5)' : '#ffff';
+  const productBg = discount ? 'rgb(151,230,5)' : '#f9bf04';
 
   const productStyles = {
-    border: '1px solid black',
-    padding: '10px',
     backgroundColor: productBg,
-    borderRadius: '10px',
   };
 
   return (
-    <div style={productStyles}>
+    <div className="product" style={productStyles}>
       <img
         src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
         alt="Tacos With Lime"
         width="640"
+        className="productImg"
       />
       <h2>{title}</h2>
       {discount ? (
-        <h3> Discount: {discount}$</h3>
+        <h3 className="discountBage"> Discount: {discount}$</h3>
       ) : (
         <p>Sorry, but discount on this type of product has expired !</p>
       )}
       <p>{price}$</p>
-      <button type="button">Add to cart</button>
+      <button className="productAddToCartBtn" type="button">
+        Add to cart
+      </button>
     </div>
   );
 };
